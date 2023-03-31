fbe44a50bedae6420394f8be7b88b4529e7081e1
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2023-04-01 03:26:45 +0900

    Update

--- src/Component/MicroCMSMyProfile.tsx
+++ src/Component/MicroCMSMyProfile.tsx
@@ -10,9 +10,9 @@ export default async function MicroCMSMyProfile(){
             <div /* tabIndex={0} className="collapse collapse-arrow collapse-open" */>
                 {/* <input type="checkbox" className="peer" />  */}
                 {/* <label className="collapse-title text-xl pb-3">{contents[0].title}</label> */}
-                <label className="text-xl pb-3">{contents[1].title}</label>
+                <label className="text-xl pb-3 card-title">{contents[1].title}</label>
                 {/* <div className="prose collapse-content" tabIndex={0}>{parse(contents[0].text)}</div> */}
-                <div /* className="prose" */>{parse(contents[1].text)}</div>
+                <div className="prose">{parse(contents[1].text)}</div>
             </div>
             
         </>
